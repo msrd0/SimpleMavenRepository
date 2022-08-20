2838fc7b6d5e422a62cec0bc3c41bc3a6a1c4165
Dominic <git@msrd0.de>
2022-08-20 10:52:24 +0200

    reformat code

--- src/main/kotlin/io/puharesource/simplemavenrepo/Authentication.kt
+++ src/main/kotlin/io/puharesource/simplemavenrepo/Authentication.kt
@@ -7,44 +7,46 @@ import java.util.Base64
 import spark.*
 
 class Authentication(usersFile: File) {
-    val users: MutableMap<String, String>
-
-    init {
-        if (!usersFile.exists()) {
-            usersFile.createNewFile()
-            usersFile.writeText("{}")
-        }
-
-        users = gson.fromJson(usersFile.readText(), gsonTypeToken<MutableMap<String, String>>())
-    }
-
-    fun handle(request: Request, response: Response) {
-        if (!request.isAuthenticated()) {
-            response.header("WWW-Authenticate", "Basic")
-            Spark.halt(401)
-        }
-    }
-
-    private fun getEncodedHeader(request: Request): String? = request.headers("Authorization")?.substringAfter("Basic ")
-
-    private fun decodeHeader(encodedHeader: String): String = Base64.getDecoder().decode(encodedHeader).toString(Charset.defaultCharset())
-
-    private fun getCredentialsFromHeader(header: String): Pair<String, String>? {
-        val credentials = header.split(":")
-
-        if (credentials.size != 2) {
-            return null
-        }
-
-        return credentials[0] to credentials[1]
-    }
-
-    private fun isAuthenticated(credentials: Pair<String, String>): Boolean = users[credentials.first] == credentials.second
-
-    private fun Request.isAuthenticated(): Boolean {
-        val encodedHeader = getEncodedHeader(this) ?: return false
-        val credentials = getCredentialsFromHeader(decodeHeader(encodedHeader)) ?: return false
-
-        return isAuthenticated(credentials)
-    }
+	val users: MutableMap<String, String>
+	
+	init {
+		if (!usersFile.exists()) {
+			usersFile.createNewFile()
+			usersFile.writeText("{}")
+		}
+		
+		users = gson.fromJson(usersFile.readText(), gsonTypeToken<MutableMap<String, String>>())
+	}
+	
+	fun handle(request: Request, response: Response) {
+		if (!request.isAuthenticated()) {
+			response.header("WWW-Authenticate", "Basic")
+			Spark.halt(401)
+		}
+	}
+	
+	private fun getEncodedHeader(request: Request): String? = request.headers("Authorization")?.substringAfter("Basic ")
+	
+	private fun decodeHeader(encodedHeader: String): String =
+		Base64.getDecoder().decode(encodedHeader).toString(Charset.defaultCharset())
+	
+	private fun getCredentialsFromHeader(header: String): Pair<String, String>? {
+		val credentials = header.split(":")
+		
+		if (credentials.size != 2) {
+			return null
+		}
+		
+		return credentials[0] to credentials[1]
+	}
+	
+	private fun isAuthenticated(credentials: Pair<String, String>): Boolean =
+		users[credentials.first] == credentials.second
+	
+	private fun Request.isAuthenticated(): Boolean {
+		val encodedHeader = getEncodedHeader(this) ?: return false
+		val credentials = getCredentialsFromHeader(decodeHeader(encodedHeader)) ?: return false
+		
+		return isAuthenticated(credentials)
+	}
 }
--- src/main/kotlin/io/puharesource/simplemavenrepo/SimpleMavenRepo.kt
+++ src/main/kotlin/io/puharesource/simplemavenrepo/SimpleMavenRepo.kt
@@ -6,49 +6,53 @@ import spark.Spark.*
 import spark.resource.AbstractFileResolvingResource
 import spark.staticfiles.MimeType
 
-internal val gson : Gson = GsonBuilder().setPrettyPrinting().create()
+internal val gson: Gson = GsonBuilder().setPrettyPrinting().create()
 
-private class FileName(private val name: String): AbstractFileResolvingResource() {
-    override fun getFilename() = name
-    
-    override fun getInputStream() = throw UnsupportedOperationException()
-    override fun getDescription() = throw UnsupportedOperationException()
+private class FileName(private val name: String) : AbstractFileResolvingResource() {
+	override fun getFilename() = name
+	
+	override fun getInputStream() = throw UnsupportedOperationException()
+	override fun getDescription() = throw UnsupportedOperationException()
 }
 
 internal fun mimeType(filename: String) = MimeType.fromResource(FileName(filename))
 
 fun main() {
-    // Load config file
-    val config = Config.loadConfig(File("config.json"))
-
-    // Create directories / files
-    val repositoryDirectory = File(config.storagePath)
-    val tmpDirectory = File("tmp")
-    val usersFile = File("users.json")
-    val defaultPomFile = File("default_pom.xml")
-
-    if (!repositoryDirectory.exists()) {
-        repositoryDirectory.mkdirs()
-    }
-
-    if (!tmpDirectory.exists()) {
-        repositoryDirectory.mkdirs()
-    }
-
-    if (!defaultPomFile.exists()) {
-        defaultPomFile.createNewFile()
-
-        defaultPomFile.writeText(Authentication::class.java.getResourceAsStream("/default_pom.xml")!!.bufferedReader().readText())
-    }
-
-    port(config.port)
-
-    Routes.registerRoutes(
-            config = config,
-            authentication = Authentication(usersFile),
-            tmpDirectory = tmpDirectory,
-            repositoryDirectory = repositoryDirectory,
-            adminCss = Authentication::class.java.getResourceAsStream("/admin.css")!!.bufferedReader().readText(),
-            defaultPom = defaultPomFile.readText()
-    )
+	// Load config file
+	val config = Config.loadConfig(File("config.json"))
+	
+	// Create directories / files
+	val repositoryDirectory = File(config.storagePath)
+	val tmpDirectory = File("tmp")
+	val usersFile = File("users.json")
+	val defaultPomFile = File("default_pom.xml")
+	
+	if (!repositoryDirectory.exists()) {
+		repositoryDirectory.mkdirs()
+	}
+	
+	if (!tmpDirectory.exists()) {
+		repositoryDirectory.mkdirs()
+	}
+	
+	if (!defaultPomFile.exists()) {
+		defaultPomFile.createNewFile()
+		
+		defaultPomFile.writeText(
+			Authentication::class.java.getResourceAsStream("/default_pom.xml")!!
+				.bufferedReader()
+				.readText()
+		)
+	}
+	
+	port(config.port)
+	
+	Routes.registerRoutes(
+		config = config,
+		authentication = Authentication(usersFile),
+		tmpDirectory = tmpDirectory,
+		repositoryDirectory = repositoryDirectory,
+		adminCss = Authentication::class.java.getResourceAsStream("/admin.css")!!.bufferedReader().readText(),
+		defaultPom = defaultPomFile.readText()
+	)
 }
